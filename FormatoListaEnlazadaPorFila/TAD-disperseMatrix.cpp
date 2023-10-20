8cd1fbbb300243a4f2d6a0abb0efd88453cf8146
Isabela <isabelagutierrez@javerianacali.edu.co>
2023-10-19 22:06:20 -0500

    Implementacion formato lista enlazada

--- FormatoListaEnlazadaPorFila/TAD-disperseMatrix.cpp
+++ FormatoListaEnlazadaPorFila/TAD-disperseMatrix.cpp
@@ -1,2 +1,102 @@
-/*La primera entrega debe contener el prototipo de la clase asociada al TAD
-DisperseMatrix así como evidenciar avances en la implementación de algunas operaciones. Esta entrega debe ser enviada a más tardar el día 21 de Octubre a las 23:59*/
+#include "TAD-disperseMatrix.h"
+
+/* Implementacion constructores */
+
+DisperseMatrix :: DisperseMatrix(){}
+
+/* Implementacion operaciones */
+
+vector <vector <Nodo>> DisperseMatrix :: rebuild(int **matriz, int fila, int columna){
+
+    int recorrerFilas, recorrerColumnas;
+    for(recorrerFilas = 0; recorrerFilas < fila; recorrerFilas++){
+
+        vector <Nodo> listaUno;
+        Nodo *tmp = act;
+
+        for(recorrerColumnas = 0; recorrerColumnas < columna; recorrerColumnas++){
+
+            if(matriz[recorrerFilas][recorrerColumnas] != 0){
+                Nodo *nuevo = new Nodo;
+                nuevo->dato = matriz[recorrerFilas][recorrerColumnas];
+
+                if(tmp == NULL){
+                    tmp = nuevo;
+                    nuevo->sig = NULL;
+                } else {
+                    tmp->sig = nuevo;
+                    tmp = tmp->sig;
+                }
+
+                listaUno.push_back(*nuevo);
+            }
+        }
+
+        listaEnlazada.push_back(listaUno);
+    }
+
+    return listaEnlazada;
+}
+
+int DisperseMatrix :: get(int **matriz, int fila, int columna){
+    int valor = 0;
+    valor = matriz[fila][columna];
+    return valor;
+}
+
+vector <Nodo> DisperseMatrix :: getRow(int fila){
+
+    vector <Nodo> valoresFila;
+
+    int recorrerColumna;
+    for(recorrerColumna = 0; recorrerColumna < listaEnlazada[fila].size(); recorrerColumna++){
+        valoresFila.push_back(listaEnlazada[fila][recorrerColumna]);
+    }
+
+    return valoresFila;
+}
+
+vector <Nodo> DisperseMatrix :: getCol(int columna){
+
+    vector <Nodo> valoresColumna;
+
+    int recorrerFila;
+    for(recorrerFila = 0; recorrerFila < 4; recorrerFila++){
+        if(listaEnlazada[recorrerFila][columna].dato != 0){
+            valoresColumna.push_back(listaEnlazada[recorrerFila][columna]);
+        }
+    }
+
+    return valoresColumna;
+}
+
+int main(){
+
+    int fila = 8;
+    int columna = 7;
+    int **matriz = new int *[fila];
+
+    matriz[0] = new int[7]{0, 2, 0, 0, 0, 0, 4};
+    matriz[1] = new int[7]{0, 8, 9, 0, 0, 1, 0};
+    matriz[2] = new int[7]{0, 0, 0, 3, 0, 0, 0};
+    matriz[3] = new int[7]{0, 0, 0, 0, 0, 0, 0};
+    matriz[4] = new int[7]{5, 0, 0, 0, 0, 6, 0};
+    matriz[5] = new int[7]{1, 2, 0, 0, 0, 0, 0};
+    matriz[6] = new int[7]{4, 0, 0, 0, 0, 0, 0};
+    matriz[7] = new int[7]{0, 0, 7, 0, 0, 11, 0};
+
+    vector <vector <Nodo>> nuevaMatriz;
+    DisperseMatrix matrizDispersa;
+
+    matrizDispersa.rebuild(matriz, fila, columna);
+    
+    vector <Nodo> filaMatriz;
+    filaMatriz = matrizDispersa.getCol(0);
+
+    int a;
+    for(a = 0; a < filaMatriz.size(); a++){
+        cout << filaMatriz[a].dato << " "; 
+    }
+
+    return 0;
+}
--- FormatoListaEnlazadaPorFila/TAD-disperseMatrix.h
+++ FormatoListaEnlazadaPorFila/TAD-disperseMatrix.h
@@ -4,10 +4,11 @@
  *
  * Representacion Estructura disperseMatrix
  * Formato: Lista enlazada por fila
- *     list<int> distintoDeCeros;
-    list<int> fila;
-    list<int> columna;
- */
+ * list<int> distintoDeCeros;
+ * list<int> fila;
+ * list<int> columna;
+*/
+
 #ifndef DISPERSE_MATRIX
 #define DISPERSE_MATRIX
 
@@ -17,43 +18,54 @@
 #include <list>
 using namespace std;
 
-class DisperseMatrix
-{
-private:
-    list<int> distintoDeCeros;
-    list<int> fila;
-    list<int> columna;
-
-public:
-    /*Constructores*/
-    DisperseMatrix(int** matriz, int fila, int columna);
-    DisperseMatrix(vector<vector<int>>& arreglo);
-    DisperseMatrix(DisperseMatrix& a);
-
-    /*Operaciones*/
-    vector<vector<int>> rebuild(vector<vector<int>> matriz, int fila, int columna);
-    int get(int fila, int columna);
-    vector<int> getRow(int fila);
-    list<int> getRowList(int fila);
-    vector<int> getCol(int columna);
-    list<int> getColList(int columna);
-    vector<int> getDisperseRow(int fila);
-    list<int> getDisperseRowList(int fila);
-    vector<int> getDisperseCol(int columna);
-    list<int> getDisperseColList(int columna);
-    int getMax();
-    int getTranspose(); 
-    void assign(int i, int j, int nuevoValor);
-    void add(vector<vector<int>> matriz);
-    void printMatrix(string separacion);
-    void productVector(vector<int> vector);
-    /*Estaticas*/
-    static DisperseMatrix addMatrixLista(list<DisperseMatrix> &lista);
-
-    /*Operadores sobrecargados*/
-    bool operator*(DisperseMatrix &num);
-    bool operator+(DisperseMatrix &num);
-    bool operator==(DisperseMatrix &num);
-
+class Nodo {
+    public:
+    int dato;
+    Nodo *sig;
 };
+
+class DisperseMatrix {
+    private:
+        Nodo *act = NULL;
+        list <int> fila;
+        list <int> columna;
+        vector <vector <Nodo>> listaEnlazada;
+
+    public:
+
+        /* Constructores */
+        DisperseMatrix();
+        DisperseMatrix(int **matriz, int fila, int columna);
+        DisperseMatrix(vector <vector <int>> &arreglo);
+        DisperseMatrix(DisperseMatrix &a);
+
+        /* Operaciones */
+        vector <vector <Nodo>> rebuild(int **matriz, int fila, int columna);
+        int get(int **matriz, int fila, int columna);
+        vector <Nodo> getRow(int fila);
+        list <int> getRowList(int fila);
+        vector <Nodo> getCol(int columna);
+        list <int> getColList(int columna);
+        vector <int> getDisperseRow(int fila);
+        list <int> getDisperseRowList(int fila);
+        vector <int> getDisperseCol(int columna);
+        list <int> getDisperseColList(int columna);
+        void printMatrix(string separacion);
+        int getMax();
+        int getTranspose();
+
+        /* Modificadoras */
+        void assign(int i, int j, int nuevoValor);
+        void add(vector <vector <int>> matriz);
+        void productVector(vector <int> vector);
+        
+        /* Estaticas */
+        static DisperseMatrix addMatrixLista(list <DisperseMatrix> &lista);
+
+        /* Sobrecarga de operadores */
+        bool operator * (DisperseMatrix &num);
+        bool operator + (DisperseMatrix &num);
+        bool operator == (DisperseMatrix &num);
+};
+
 #endif
